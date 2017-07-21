pypa/pip

Add a common_wheels fixtures providing wheel & setuptools wheels

tests/conftest.py
@@ -1,6 +1,7 @@
 import io
 import os
 import shutil
+import subprocess
 import sys
 
 import pytest
@@ -186,6 +187,18 @@ def script(tmpdir, virtualenv):
         capture_temp=True,
         assert_no_temp=True,
     )
+
+
+@pytest.fixture(scope="session")
+def common_wheels_dir(tmpdir_factory):
+    """Provide a directory with latest setuptools and wheel wheels"""
+    wheels_dir = tmpdir_factory.mktemp('data')
+    subprocess.check_call([
+        'pip', 'download', 'wheel', 'setuptools',
+        '-d', str(wheels_dir),
+    ])
+    yield wheels_dir
+    wheels_dir.remove(ignore_errors=True)
 
 
 @pytest.fixture

aboutcode-org/pip-requirements-parser

tests/conftest.py
@@ -1,6 +1,7 @@
 import io
 import os
 import shutil
+import subprocess
 import sys
 
 import pytest
@@ -189,6 +190,18 @@ def script(tmpdir, virtualenv):
         capture_temp=True,
         assert_no_temp=True,
     )
+
+
+@pytest.fixture(scope="session")
+def common_wheels(tmpdir_factory):
+    """Provide a directory with latest setuptools and wheel wheels"""
+    wheels_dir = tmpdir_factory.mktemp('common_wheels')
+    subprocess.check_call([
+        'pip', 'download', 'wheel', 'setuptools',
+        '-d', str(wheels_dir),
+    ])
+    yield wheels_dir
+    wheels_dir.remove(ignore_errors=True)
 
 
 @pytest.fixture