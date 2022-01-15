ee2912d016ca095b369d7d238566ba3e537613cb
Philippe Ombredanne <pombredanne@nexb.com>
2022-01-15 15:56:27 +0100

    Adjust PackageFinder to new context
    Eventually this may need to be removed. For now this is mostly a
    dummy finder that does not make network calls.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/pip_requirements.py
+++ src/pip_requirements.py
@@ -650,12 +650,12 @@ class PackageFinder:
 
     def __init__(
         self,
-        link_collector: LinkCollector,
-        target_python: TargetPython,
-        allow_yanked: bool,
+        search_scope: Optional[SearchScope] = None,
+        allow_yanked: Optional[bool] = False,
         format_control: Optional[FormatControl] = None,
-        candidate_prefs: Optional[CandidatePreferences] = None,
         ignore_requires_python: Optional[bool] = None,
+        *args: Any,
+        **kwargs: Any,
     ) -> None:
         """
         This constructor is primarily meant to be used by the create() class
@@ -667,21 +667,18 @@ class PackageFinder:
         :param candidate_prefs: Options to use when creating a
             CandidateEvaluator object.
         """
-        if candidate_prefs is None:
-            candidate_prefs = CandidatePreferences()
 
         format_control = format_control or FormatControl(set(), set())
 
         self._allow_yanked = allow_yanked
-        self._candidate_prefs = candidate_prefs
+
         self._ignore_requires_python = ignore_requires_python
-        self._link_collector = link_collector
-        self._target_python = target_python
 
-        self.format_control = format_control
+        self._search_scope = search_scope or SearchScope([], [])
+        self._allow_all_prereleases =False
+        self._prefer_binary = True
 
-        # These are boring links that have already been logged somehow.
-        self._logged_links: Set[Link] = set()
+        self.format_control= format_control or FormatControl(set(), set())
 
     # Don't include an allow_yanked default value to make sure each call
     # site considers whether yanked releases are allowed. This also causes
@@ -690,73 +687,53 @@ class PackageFinder:
     @classmethod
     def create(
         cls,
-        link_collector: LinkCollector,
-        selection_prefs: SelectionPreferences,
-        target_python: Optional[TargetPython] = None,
+        *args: Any,
+        **kwargs: Any,
     ) -> "PackageFinder":
-        """Create a PackageFinder.
-
-        :param selection_prefs: The candidate selection preferences, as a
-            SelectionPreferences object.
-        :param target_python: The target Python interpreter to use when
-            checking compatibility. If None (the default), a TargetPython
-            object will be constructed from the running Python.
-        """
-        if target_python is None:
-            target_python = TargetPython()
-
-        candidate_prefs = CandidatePreferences(
-            prefer_binary=selection_prefs.prefer_binary,
-            allow_all_prereleases=selection_prefs.allow_all_prereleases,
-        )
-
-        return cls(
-            candidate_prefs=candidate_prefs,
-            link_collector=link_collector,
-            target_python=target_python,
-            allow_yanked=selection_prefs.allow_yanked,
-            format_control=selection_prefs.format_control,
-            ignore_requires_python=selection_prefs.ignore_requires_python,
-        )
+        """Create a PackageFinder."""
+        return cls()
 
     @property
-    def target_python(self) -> TargetPython:
-        return self._target_python
+    def target_python(self):
+        return
 
     @property
     def search_scope(self) -> SearchScope:
-        return self._link_collector.search_scope
+        return self._search_scope
 
     @search_scope.setter
     def search_scope(self, search_scope: SearchScope) -> None:
-        self._link_collector.search_scope = search_scope
+        self._search_scope = search_scope
 
     @property
     def find_links(self) -> List[str]:
-        return self._link_collector.find_links
+        if not self.search_scope:
+            self.search_scope = SearchScope([], [])
+        return self.search_scope.find_links
 
     @property
     def index_urls(self) -> List[str]:
+        if not self.search_scope:
+            self.search_scope = SearchScope([], [])
         return self.search_scope.index_urls
 
     @property
     def trusted_hosts(self) -> Iterable[str]:
-        for host_port in self._link_collector.session.pip_trusted_origins:
-            yield build_netloc(*host_port)
+        return
 
     @property
     def allow_all_prereleases(self) -> bool:
-        return self._candidate_prefs.allow_all_prereleases
+        return self._allow_all_prereleases
 
     def set_allow_all_prereleases(self) -> None:
-        self._candidate_prefs.allow_all_prereleases = True
+        self._allow_all_prereleases = True
 
     @property
     def prefer_binary(self) -> bool:
-        return self._candidate_prefs.prefer_binary
+        return self._prefer_binary
 
     def set_prefer_binary(self) -> None:
-        self._candidate_prefs.prefer_binary = True
+        self._prefer_binary = True
 
 # PIPREQPARSE: end from src/pip/_internal/index/package_finder.py
 ################################################################################
