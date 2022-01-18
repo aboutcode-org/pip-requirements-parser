fc534de72c481b4bbd59a3bf2be6534ff9046e7f
Philippe Ombredanne <pombredanne@nexb.com>
2022-01-19 00:00:34 +0100

    Add license notice, remove unused import
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_pip_api_parse_requirements.py
+++ tests/test_pip_api_parse_requirements.py
@@ -1,5 +1,23 @@
-from textwrap import dedent
 
+
+"""
+Copyright (c) Dustin Ingram <di@python.org> and others
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This is a modified subst of the tests from https://github.com/di/pip-api/
+adapted to work with our pip requirement parser.
+"""
 import pytest
 
 import pip_api
