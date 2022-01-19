4537d32a424932f3b2a9fcf06d0c41afdb6a848f
Philippe Ombredanne <pombredanne@nexb.com>
2022-01-19 18:06:30 +0100

    Adapt dparse tests to pass with our parser
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_dparse.py
+++ tests/test_dparse.py
@@ -3,26 +3,26 @@
 """Tests for `dparse.parser`"""
 
 # Originally from:
-#     name='dparse',
+#     name="dparse",
 #     description="A parser for Python dependency files",
 #     author="Jannis Gebauer",
-#     author_email='support@pyup.io',
-#     url='https://github.com/pyupio/dparse',
-# 
+#     author_email="support@pyup.io",
+#     url="https://github.com/pyupio/dparse",
+#
 # MIT License
-# 
+#
 # Copyright (c) 2017, Jannis Gebauer
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
 # the Software without restriction, including without limitation the rights to
 # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 # the Software, and to permit persons to whom the Software is furnished to do so,
 # subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -30,78 +30,107 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from pip_req_parse_tests.unit.test_req_file import parse_requirement_text
 
-from packaging.specifiers import SpecifierSet
 
-
-def test_requirements_with_invalid_requirement():
-
+def test_requirements_with_invalid_requirement(parse_requirement_text):
     content = "in=vali===d{}{}{"
-    dep_file = parse(content)
-    assert len(dep_file.dependencies) == 0
+    rf = parse_requirement_text(content)
+    assert rf.invalid_lines[0].requirement_line.line == content
 
 
-def test_index_server():
+def test_index_urls_1(parse_requirement_text):
     line = "--index-url https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo/"}
 
+
+def test_index_urls_2(parse_requirement_text):
     line = "-i https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo/"}
 
+
+def test_index_urls_3(parse_requirement_text):
     line = "--extra-index-url https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo/"]}
 
+
+def test_index_urls_4(parse_requirement_text):
     line = "--extra-index-url https://some.foo"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo"]}
 
+
+def test_index_urls_5(parse_requirement_text):
     line = "--extra-index-url https://some.foo # some lousy comment"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo"]}
+    assert parse_requirement_text(line).comments[0].line == "# some lousy comment"
 
+
+def test_index_urls_6(parse_requirement_text):
     line = "-i\t\t https://some.foo \t\t    # some lousy comment"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo"}
+    assert parse_requirement_text(line).comments[0].line == "# some lousy comment"
 
+
+def test_index_urls_7(parse_requirement_text):
     line = "--index-url"
-    assert Parser.parse_index_server(line) is None
+    assert parse_requirement_text(line).options == []
+    rfi = parse_requirement_text(line).invalid_lines
+    assert rfi
+    assert "--index-url option" in rfi[0].error_message
 
+
+def test_index_urls_8(parse_requirement_text):
     line = "--index-url=https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo/"}
 
+
+def test_index_urls_9(parse_requirement_text):
     line = "-i=https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo/"}
 
+
+def test_index_urls_10(parse_requirement_text):
     line = "--extra-index-url=https://some.foo/"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo/"]}
 
+
+def test_index_urls_11(parse_requirement_text):
     line = "--extra-index-url=https://some.foo"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo"]}
 
+
+def test_index_urls_12(parse_requirement_text):
     line = "--extra-index-url=https://some.foo # some lousy comment"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"extra_index_urls": ["https://some.foo"]}
+    assert parse_requirement_text(line).comments[0].line == "# some lousy comment"
 
+
+def test_index_urls_13(parse_requirement_text):
     line = "-i\t\t =https://some.foo \t\t    # some lousy comment"
-    assert Parser.parse_index_server(line) == "https://some.foo/"
+    assert parse_requirement_text(line).options[0].options == {"index_url": "https://some.foo"}
+    assert parse_requirement_text(line).comments[0].line == "# some lousy comment"
 
 
-def test_requirements_package_with_index_server():
+def test_requirements_package_with_index_server(parse_requirement_text):
     content = """-i https://some.foo/\ndjango"""
+    dep_file = parse_requirement_text(content)
+    assert dep_file.options[0].options == {"index_url": "https://some.foo/"}
 
-    dep_file = parse(content=content)
-    dep = dep_file.dependencies[0]
-
+    dep = dep_file.requirements[0]
     assert dep.name == "django"
-    assert dep.index_server == "https://some.foo/"
 
 
-def test_requirements_parse_empty_line():
+def test_requirements_parse_empty_line(parse_requirement_text):
     content = """
     """
+    dep_file = parse_requirement_text(content)
+    assert dep_file.requirements == []
+    assert dep_file.comments == []
+    assert dep_file.invalid_lines == []
+    assert dep_file.options == []
 
-    dep_file = parse(content=content)
-    assert dep_file.dependencies == []
-    assert dep_file.resolved_files == []
 
-
-def test_requirements_parse_unsupported_line_start():
+def test_requirements_parse_unsupported_line_start(parse_requirement_text):
     content = "-f foo\n" \
               "--find-links bla\n" \
               "-i bla\n" \
@@ -113,22 +142,30 @@ def test_requirements_parse_unsupported_line_start():
               "-Z\n" \
               "--always-unzip\n"
 
-    dep_file = parse(content=content)
-    assert dep_file.dependencies == []
-    assert dep_file.resolved_files == []
-
-
-def test_file_resolver():
-    content = "-r production/requirements.txt\n" \
-              "--requirement test.txt\n"
-
-    dep_file = parse(content=content, path="/")
-
-    assert dep_file.resolved_files == [
-        "/production/requirements.txt",
-        "/test.txt"
+    dep_file = parse_requirement_text(content)
+    assert dep_file.requirements == []
+    assert [o.options for o in dep_file.options] == [
+        {"find_links": ["foo"]},
+        {"find_links": ["bla"]},
+        {"index_url": "bla"},
+        {"index_url": "bla"},
+        {"extra_index_urls": ["bla"]},
+        {"no_index": True},
     ]
 
-    dep_file = parse(content=content)
+    # these are legacy pip options 
+    assert [iv.requirement_line.line for iv in dep_file.invalid_lines] == [
+        '--allow-external', '--allow-unverified', '-Z', '--always-unzip']
 
-    assert dep_file.resolved_files == []
+
+def test_file_resolver(parse_requirement_text):
+    content = (
+        "-r production/requirements.txt\n"
+        "--requirement test.txt\n"
+    )
+    dep_file = parse_requirement_text(content)
+    opts = [o.options for o in dep_file.options]
+    assert opts == [
+        {"requirements": ["production/requirements.txt"]},
+        {"requirements": ["test.txt"]},
+    ]
