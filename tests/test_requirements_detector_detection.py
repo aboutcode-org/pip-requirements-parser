eccccd39a0d97c2911137f62cfa69c2a098d857d
Philippe Ombredanne <pombredanne@nexb.com>
2022-01-20 08:46:27 +0100

    Adapt requirements-detector tests
    All test are passing
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_requirements_detector_detection.py
+++ tests/test_requirements_detector_detection.py
@@ -1,24 +1,24 @@
 
 # Originally from:
-#     name='requirements-detector',
-#     url='https://github.com/landscapeio/requirements-detector',
-#     author='landscape.io',
-#     author_email='code@landscape.io',
-# 
+#     name="requirements-detector",
+#     url="https://github.com/landscapeio/requirements-detector",
+#     author="landscape.io",
+#     author_email="code@landscape.io",
+#
 # SPDX-License-Identifier: MIT
 #
 # The MIT License
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,94 +29,53 @@
 
 import os
 from unittest import TestCase
-from requirements_detector.detect import from_requirements_txt, from_requirements_dir, \
-    from_requirements_blob, from_setup_py, CouldNotParseRequirements
-from requirements_detector.requirement import DetectedRequirement
+
+from pip_requirements import RequirementsFile
 
 
 class DependencyDetectionTest(TestCase):
 
-    def _expected(self, *requirements):
-        return [DetectedRequirement.parse(req) for req in requirements]
-
     def test_requirements_txt_parsing(self):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test1/requirements.txt')
-        dependencies = from_requirements_txt(filepath)
+        filepath = os.path.join(os.path.dirname(__file__), "requirements_detector/test1/requirements.txt")
+        dependencies = RequirementsFile(filepath)
+        results = [str(r.req) for r in dependencies.requirements]
 
-        expected = self._expected(
-            'amqp!=1.0.13',
-            'Django>=1.5.0',
-            'six<1.4,>=1.3.0',
-            'South==0.8.2',
-        )
+        expected = ["Django>=1.5.0", "South==0.8.2", "amqp!=1.0.13", "six<1.4,>=1.3.0"]
 
-        self.assertEqual(expected, sorted(dependencies))
+        assert results == expected
+        assert dependencies.options[0].options == {"index_url": "https://example.com/custom/pypi"}
+        assert dependencies.comments[0]. line == "# we want six too"
 
-    def test_requirements_dir_parsing(self):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test2/requirements')
-        dependencies = from_requirements_dir(filepath)
+    def test_requirements_dir_parsing1(self):
+        filepath = os.path.join(os.path.dirname(__file__), "requirements_detector/test2/requirements/base.txt")
+        dependencies = RequirementsFile(filepath)
+        results = [str(r.req) for r in dependencies.requirements]
 
-        expected = self._expected(
-            'amqp==1.0.13',
-            'anyjson==0.3.3',
-            'Django==1.5.2',
-            'South==0.8.2',
-        )
+        expected = [
+            "amqp==1.0.13",
+            "anyjson==0.3.3",
+        ]
 
-        self.assertEqual(expected, sorted(dependencies))
+        assert results == expected
 
-    def test_requirements_blob_parsing(self):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test3')
-        dependencies = from_requirements_blob(filepath)
+    def test_requirements_dir_parsing2(self):
+        filepath = os.path.join(os.path.dirname(__file__), "requirements_detector/test2/requirements/webui.pip")
+        dependencies = RequirementsFile(filepath)
+        results = [str(r.req) for r in dependencies.requirements]
 
-        expected = self._expected(
-            'amqp==1.0.13',
-            'anyjson==0.3.3',
-            'django-gubbins==1.1.2',
-        )
+        expected = [
+            "Django==1.5.2",
+            "South==0.8.2",
+        ]
 
-        self.assertEqual(expected, sorted(dependencies))
+        assert results == expected
 
     def test_invalid_requirements_txt(self):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test5/invalid_requirements.txt')
-        dependencies = from_requirements_txt(filepath)
-        expected = self._expected('django<1.6', 'django')
-        self.assertEqual(expected, sorted(dependencies))
+        filepath = os.path.join(os.path.dirname(__file__), "requirements_detector/test5/invalid_requirements.txt")
+        dependencies = RequirementsFile(filepath)
+        results = [str(r.req) for r in dependencies.requirements]
 
-    def test_invalid_requirements_txt(self):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test6/requirements.txt')
-        from_requirements_txt(filepath)
-
-    def _test_setup_py(self, setup_py_file, *expected):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test4', setup_py_file)
-        dependencies = from_setup_py(filepath)
-        expected = self._expected(*expected)
-        self.assertEqual(expected, sorted(dependencies))
-
-    def _test_setup_py_not_parseable(self, setup_py_file):
-        filepath = os.path.join(os.path.dirname(__file__), 'detection/test4', setup_py_file)
-        self.assertRaises(CouldNotParseRequirements, from_setup_py, filepath)
-
-    def test_simple_setup_py_parsing(self):
-        self._test_setup_py('simple.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_setup_py_reqs_defined_in_file_parsing(self):
-        self._test_setup_py('in_file.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_setup_py_tuple(self):
-        self._test_setup_py('tuple.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_subscript_assign(self):
-        self._test_setup_py('subscript_assign.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_utf8_setup_py(self):
-        self._test_setup_py('utf8.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_requires_setup_py(self):
-        self._test_setup_py('uses_requires.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_requires_and_install_requires_setup_py(self):
-        self._test_setup_py('uses_requires_and_install_requires.py', 'Django==1.5.0', 'django-gubbins==1.1.2')
-
-    def test_callable_install_requires(self):
-        self._test_setup_py_not_parseable('callable.py')
+        expected = ["django<1.6", "django"]
+        assert results == expected
+        errors = [i.requirement_line.line for i in dependencies.invalid_lines]
+        assert errors == ["<<<<<<< HEAD", "=======", ">>>>>>>"]
--- tests/test_requirements_detector_parsing.py
+++ tests/test_requirements_detector_parsing.py
@@ -1,24 +1,24 @@
 
 # Originally from:
-#     name='requirements-detector',
-#     url='https://github.com/landscapeio/requirements-detector',
-#     author='landscape.io',
-#     author_email='code@landscape.io',
+#     name="requirements-detector",
+#     url="https://github.com/landscapeio/requirements-detector",
+#     author="landscape.io",
+#     author_email="code@landscape.io",
 
 # SPDX-License-Identifier: MIT
-# 
+#
 # The MIT License
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,102 +27,169 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
-try:
-    import urlparse
-except ImportError:
-    # python3
-    from urllib import parse as urlparse
-
+import tempfile
 from unittest import TestCase
-from requirements_detector.requirement import DetectedRequirement, _parse_egg_name, _strip_fragment
+
+from pip_requirements import RequirementsFile
+
+from pip_req_parse_tests.lib.path import Path
+from pip_req_parse_tests.lib import requirements_file
+from pip_req_parse_tests.unit.misc import rmtree
 
 
 class TestRequirementParsing(TestCase):
 
-    def _test(self, requirement, name=None, version_specs=None, url=None):
-        req = DetectedRequirement.parse(requirement)
-        self.assertEqual(name, req.name)
-        if version_specs is None:
-            self.assertEqual([], req.version_specs)
-        else:
-            for spec in version_specs:
-                self.assertTrue(spec in req.version_specs)
-        self.assertEqual(url, req.url)
+    def setUp(self) -> None:
+        self.tmpdir = Path(str(tempfile.mkdtemp()))
+
+    def tearDown(self) -> None:
+        rmtree(self.tmpdir)
+
+    def _check_req(
+        self,
+        requirement,
+        name=None,
+        version_specs=None,
+        url=None,
+        comment=None
+    ):
+        """
+        Parse ``requirement`` as a RequirementsFile and check the provided
+        arguments against the parserd results.
+        """
+        with requirements_file(contents=requirement, tmpdir=self.tmpdir) as rf:
+            reqf = RequirementsFile(rf)
+            assert len(reqf.requirements) == 1
+            assert not reqf.invalid_lines
+            assert not reqf.options
+
+            req = reqf.requirements[0]
+            assert req.name == name
+
+            if not version_specs:
+                assert not req.specifier
+            else:
+                expected = sorted((vers, op) for op, vers in version_specs)
+                result = sorted((spec.version, spec.operator) for spec in req.specifier)
+                assert result == expected
+
+            if url:
+                assert req.link.url
+            else:
+                assert not req.link
+
+            if comment:
+                assert len(reqf.comments) == 1
+                assert reqf.comments[0].line == comment
 
     def test_basic_requirement(self):
-        self._test('Django', 'django')
-        self._test('celery', 'celery')
-
+        self._check_req(requirement="Django", name="Django")
+        self._check_req(requirement="celery", name="celery")
 
     def test_requirement_with_versions(self):
-        self._test('Django==1.5.2', 'django', [('==', '1.5.2')])
-        self._test('South>0.8', 'south', [('>', '0.8')])
-        self._test('django-gubbins!=1.1.1,>1.1', 'django-gubbins', [('!=', '1.1.1'), ('>', '1.1')])
+        self._check_req(
+            requirement="Django==1.5.2",
+            name="Django",
+            version_specs=[("==", "1.5.2")],
+        )
+
+    def test_requirement_with_versions2(self):
+        self._check_req(
+            requirement="South>0.8",
+            name="South",
+            version_specs=[(">", "0.8")],
+        )
+
+    def test_requirement_with_versions3(self):
+        self._check_req(
+            requirement="django-gubbins!=1.1.1,>1.1",
+            name="django-gubbins",
+            version_specs=[("!=", "1.1.1"), (">", "1.1")],
+        )
 
     def test_relative_file_path(self):
-        self._test('../somelib', url='../somelib')
+        self._check_req(requirement="../somelib", url="../somelib")
 
     def test_vcs_url(self):
-        self._test('git+ssh://git@github.com/something/somelib.git',
-                   url='git+ssh://git@github.com/something/somelib.git')
-        self._test('git+ssh://git@github.com/something/somelib.git#egg=somelib',
-                   name='somelib', url='git+ssh://git@github.com/something/somelib.git')
-        self._test('git://github.com/peeb/django-mollie-ideal.git#egg=mollie',
-                   name='mollie', url='git+git://github.com/peeb/django-mollie-ideal.git')
+        self._check_req(
+            requirement="git+ssh://git@github.com/something/somelib.git",
+            url="git+ssh://git@github.com/something/somelib.git",
+        )
+
+    def test_vcs_url1(self):
+        self._check_req(
+            requirement="git+ssh://git@github.com/something/somelib.git#egg=somelib",
+            name="somelib",
+            url="git+ssh://git@github.com/something/somelib.git",
+        )
+
+    def test_vcs_url2(self):
+        self._check_req(
+            requirement="git://github.com/peeb/django-mollie-ideal.git#egg=mollie",
+            name="mollie",
+            url="git+git://github.com/peeb/django-mollie-ideal.git",
+        )
 
     def test_archive_url(self):
-        self._test('http://example.com/somelib.tar.gz', url='http://example.com/somelib.tar.gz')
-        self._test('http://example.com/somelib.tar.gz#egg=somelib', name='somelib',
-                   url='http://example.com/somelib.tar.gz')
+        self._check_req(
+            requirement="http://example.com/somelib.tar.gz",
+            url="http://example.com/somelib.tar.gz",
+        )
+
+        self._check_req(
+            requirement="http://example.com/somelib.tar.gz#egg=somelib",
+            name="somelib",
+            url="http://example.com/somelib.tar.gz",
+        )
 
     def test_editable_relative_path(self):
-        self._test('-e ../somelib', url='../somelib')
+        self._check_req(
+            requirement="-e ../somelib",
+            url="../somelib",
+        )
 
     def test_editable_vcs_url(self):
-        self._test('--editable git+ssh://git@github.com/something/somelib.git#egg=somelib',
-                   name='somelib', url='git+ssh://git@github.com/something/somelib.git')
-
-    def test_comments(self):
-        self._test('celery == 0.1 # comment', 'celery', [('==', '0.1')])
-        self._test('celery == 0.1\t# comment', 'celery', [('==', '0.1')])
-        self._test(
-            "somelib == 0.15 # pinned to 0.15 (https://github.com/owner/repo/issues/111)",
-            "somelib",
-            [("==", "0.15")]
-        )
-        self._test(
-            'http://example.com/somelib.tar.gz # comment',
-            url='http://example.com/somelib.tar.gz'
-        )
-        self._test(
-            'http://example.com/somelib.tar.gz#egg=somelib # url comment http://foo.com/bar',
-            name='somelib',
-            url='http://example.com/somelib.tar.gz'
+        self._check_req(
+            requirement="--editable git+ssh://git@github.com/something/somelib.git#egg=somelib",
+            name="somelib",
+            url="git+ssh://git@github.com/something/somelib.git",
         )
 
+    def test_comments1(self):
+        self._check_req(
+            requirement="celery == 0.1 # comment",
+            name="celery",
+            version_specs=[("==", "0.1")],
+            comment="# comment",
+        )
 
-class TestEggFragmentParsing(TestCase):
+    def test_comments2(self):
+        self._check_req(
+            requirement="celery == 0.1\t# comment",
+            name="celery",
+            version_specs=[("==", "0.1")],
+            comment="# comment",
+        )
 
-    def test_simple(self):
-        self.assertEqual('somelib', _parse_egg_name('egg=somelib'))
+    def test_comments3(self):
+        self._check_req(
+            requirement="somelib == 0.15 # pinned to 0.15 (https://github.com/owner/repo/issues/111)",
+            name="somelib",
+            version_specs=[("==", "0.15")],
+            comment="# pinned to 0.15 (https://github.com/owner/repo/issues/111)",
+        )
 
-    def test_no_egg_value(self):
-        self.assertTrue(_parse_egg_name('a=b&c=2') is None)
+    def test_comments4(self):
+        self._check_req(
+            requirement="http://example.com/somelib.tar.gz # comment",
+            url="http://example.com/somelib.tar.gz",
+            comment="# comment",
+        )
 
-    def test_no_pairs(self):
-        self.assertTrue(_parse_egg_name('somelib') is None)
-
-    def test_first_egg_val(self):
-        self.assertEqual('somelib', _parse_egg_name('egg=somelib&egg=anotherlib'))
-
-    def test_multiple_fragment_values(self):
-        self.assertEqual('somelib', _parse_egg_name('a=1&egg=somelib&b=2'))
-
-
-class TestFragmentStripping(TestCase):
-
-    def test_stripping(self):
-        url = 'http://example.com/index.html?a=b&c=2#some_fragment'
-        parts = urlparse.urlparse(url)
-        self.assertEqual('http://example.com/index.html?a=b&c=2', _strip_fragment(parts))
+    def test_comments5(self):
+        self._check_req(
+            requirement="http://example.com/somelib.tar.gz#egg=somelib # url comment http://foo.com/bar",
+            name="somelib",
+            url="http://example.com/somelib.tar.gz",
+            comment="# url comment http://foo.com/bar",
+        )
